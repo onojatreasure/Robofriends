55ae6ee96b8c4aba8c6cf154762fb786ece254fe
Onoja Treasure <onojatreasure@gmail.com>
2019-07-06 01:04:50 +0100

    Built search feature

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Treasure Onoja',
+        username: 'Roman',
+        email: 'onojatreasure@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Victor Edward',
+        username: 'Rude',
+        email: 'rudevice@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Marley Francis',
+        username: 'Marley',
+        email: 'francis@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Hannah Hyde',
+        username: 'Hannah',
+        email: 'hannah.hyde.com'
+    },
+    {
+        id: 5,
+        name: 'Nanret Isuwa',
+        username: 'Mama',
+        email: 'isuwa@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Norhile James',
+        username: 'Papa',
+        email: 'norhileJames@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Victoria Inikpi',
+        username: 'Vicy',
+        email: 'victoria.ionikpi@gmail.com'
+    }
+];
