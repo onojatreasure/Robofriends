45ecba8662bdb96474504aadc5cc3e1a19633f6d
Onoja Treasure <onojatreasure@gmail.com>
2019-07-20 15:55:01 +0100

    Just noticed that the entire cardlist componenet is inside the scrollbar. Now I neeed to pull it out and find another way to scroll

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,7 +3,7 @@ import Card from './Card';
 
 const CardList = ({robots}) => {
     return (
-        <div>
+        <div style={{overflow: 'auto'}}>
     {
     robots.map((user, i) => {
         return  (
--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', border: '5px solid black', height: '600px'}}>
+        <div style={{overflowY: 'auto', border: '5px solid black', height: '500px', borderRadius: '1px'}}>
             { props.children }
         </div>
     );
