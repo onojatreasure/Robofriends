0357047a6cdbf8016a469bd3bf33139d99d01e39
Onoja Treasure <onojatreasure@gmail.com>
2019-08-09 02:09:07 +0100

    Added another middleware and did amazing things with redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider, connect } from 'react-redux';
-import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
+import { createLogger } from 'redux-logger';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
 import 'tachyons';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots})
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
     <Provider  store={store}>
